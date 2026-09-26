d68bb4872eb4a3cab7f43021032cb4a8a99cd9f5
Tatsuro <tatsuro@Micheles-MacBook-Air.local>
2026-09-26 22:24:14 +0800

    一覧の左スワイプで既読にする

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // index.html / sw.js を変えたら CACHE の版番号を必ず上げること
-const CACHE = "kiosk-v13";
+const CACHE = "kiosk-v14";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
